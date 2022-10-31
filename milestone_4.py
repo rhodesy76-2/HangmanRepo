9a275f9ef7f915e25adf569cc604a991354615fd
rhodesy76-2 <113443029+rhodesy76-2@users.noreply.github.com>
2022-10-31 13:50:16 +0100

    Defined what happens if the lketter is in the word

--- milestone_4.py
+++ milestone_4.py
@@ -2,27 +2,29 @@
 # Import random module
 import random
 
-# Word List to play with
-# word_list = ["banana", "apple", "kiwi", "orange", "pear"]
-# print(word_list)
-
-# Takes word list and randomly chooses word
+# escape in input as isalpha is called, how to recognize still?
 
 
 class Hangman():
     def __init__(self, word_list, num_lives=5):
-        self.word =  random.choice(word_list)
-        self.word_guessed = list
-        self.num_letters = int
-        self.num_lives = num_lives
-        self.word_list = word_list
-        self.list_of_guesses = []
+        self.word =  random.choice(word_list) # The word to be guessed, picked randomly from the word_list, using the random funcrtiun (imported at start of program)
+        self.word_guessed = ["_" for i in self.word] # Takesr andom word, from self.word, create new list, guessed_word, replacing each letter in the word with a _ for each letter not guesssed
+        self.num_letters = len(set(self.word)) # The number of UNIQUE letters in the word that have not been guessed yet, by using len on the set (unique letters) of the randown word
+        self.num_lives = num_lives # The number of lives the player has at the start of the game, a default parameter and the value is set to 5.
+        self.word_list = word_list # A list of words, set in init variable word_list
+        self.list_of_guesses = [] # List of guesssed letters, set to empty initially
         print(self.__dict__)
-        
+
+            
+               
     def check_guess(self, guess):
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess {guess} is in the word.")
+            for ind, char in enumerate(self.word):
+                if char == guess:
+                   self.word_guessed[ind] =  char
+            self.num_letters -= 1
         else:
             print(f"Sorry, {guess} is not in the word. Try again.")
             
@@ -38,7 +40,7 @@ class Hangman():
                 print(f"You guesssed {guess}")
                 # Calls the check guess function with the single alphabetical guess
                 self.check_guess(guess)
-                self.list_of_guesses = (self.list_of_guesses).append(guess)
+                self.list_of_guesses.append(guess)
                 print(self.__dict__)
               
 # argument of type 'NoneType' is not iterable error and also, keeps trying to take guesses?
